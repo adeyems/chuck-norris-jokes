b31b985a9b5ac8a4c31b3a5b9960410b2a80f594
ADEYEMO Qudus <adeyems@gmail.com>
2020-06-08 13:42:10 +0100

    Apply fixes from StyleCI (#3)

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -1,32 +1,23 @@
 <?php
 
-
 namespace Qudus\ChuckNorrisJokes;
 
 use Illuminate\Support\ServiceProvider;
 use Qudus\ChuckNorrisJokes\Jokes\JokeFactory;
 
 /**
- * Class ChuckNorrisJokesServiceProvider
- * @package Qudus\ChuckNorrisJokes
+ * Class ChuckNorrisJokesServiceProvider.
  */
 class ChuckNorrisJokesServiceProvider extends ServiceProvider
 {
-    /**
-     *
-     */
-    public function boot(){
-
+    public function boot()
+    {
     }
 
-
-    /**
-     *
-     */
-    public function register(){
-        return $this->app->bind('chuck-norris', function (){
-           return new JokeFactory();
+    public function register()
+    {
+        return $this->app->bind('chuck-norris', function () {
+            return new JokeFactory();
         });
     }
-
 }
